2d31e365f4f8d6ff2a9612607faea7deff67d86f
rivalmsr <rivalmusripal@gmail.com>
2021-09-10 23:02:47 +0700

    fix product component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Produts from './components/Products';
 
 const App = () => {
   return (
     <div>
-      Welcome React!
+      <Produts />
     </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
+import Product from './Product/Product'
+
 const products = [
-  {id: 1, name: 'Shoes', description: 'Running shoes.'},
-  {id: 2, name: 'Macbook', description: 'Apple macbook.'}
+  {id: 1, name: 'Shoes', description: 'Running shoes.', image: 'https://media.dinomarket.com/docs/imgTD/2020-11/pic_macbook_m1_Grey1_121120221149_ll.jpg.jpg'},
+  {id: 2, name: 'Macbook', description: 'Apple macbook.', image: 'https://media.dinomarket.com/docs/imgTD/2020-11/pic_macbook_m1_Grey1_121120221149_ll.jpg.jpg'}
 ];
 
 const Products = () => {
-  <main>
-    <Grid container justify="center" spacing={4}>
-      { products.map((product)=> {
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <Product product={product} />
-        </Grid>
-        })
-      }
-    </Grid>
-  </main>
+  return (
+    <main>
+      <Grid container justifyContent="center" spacing={4}>
+        { products.map((product)=> (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Product product={product} />
+            </Grid>
+          ))
+        }
+      </Grid>
+    </main>
+  )
 }
 
 export default Products;
