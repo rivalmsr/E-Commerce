551252fd3e81a69ff73f0586e4eeaa28d75b8f3a
rivalmsr <rivalmusripal@gmail.com>
2021-09-16 23:40:40 +0700

    add commerce lib

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { Navbar, Products} from './components';
+import { commerce } from './lib/commerce';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log('%c -> here!', 'color:orange;');
+  console.log(products);
+
   return (
     <div>
       <Navbar />
